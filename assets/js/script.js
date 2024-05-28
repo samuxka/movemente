d975810f7ee80b09f9a06b142a541b2c5fd1001f
samuxka <samukactto@gmail.com>
2024-05-28 16:26:34 -0300

    cabei

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,58 @@
 const header = document.querySelector(".navbar")
 
-window.addEventListener("scroll", function(){
-    header.classList.toggle("sticky", window.scrollY > 60)
+        window.addEventListener("scroll", function() {
+            header.classList.toggle("sticky", window.scrollY > 60)
+        });
 
-})
+        window.onscroll = function() {scrollFunction()};
 
-window.onscroll = function() {scrollFunction()};
+        function scrollFunction(){
+            const backToTopBtn = document.getElementById("backToTopBtn")
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+                backToTopBtn.style.display = "block"
+            } else {
+                backToTopBtn.style.display = "none"
+            }
+        }
 
-function scrollFunction(){
-    const backToTopBtn = document.getElementById("backToTopBtn")
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-        backToTopBtn.style.display = "block"
-    }else{
-        backToTopBtn.style.display = "none"
-    }
-}
+        function topFunction(){
+            document.body.scrollTop = 0
+            document.documentElement.scrollTop = 0
+        }
 
-function topFunction(){
-    document.body.scrollTop = 0
-    document.documentElement.scrollTop = 0
-}
+        document.getElementById('menu-icon').addEventListener('click', function() {
+            var navList = document.querySelector('.navlist');
+            var navbar = document.querySelector('.navbar');
+            navList.classList.toggle('show');
+            navbar.classList.toggle('show');
+        });
+
+        document.querySelectorAll('.navlist a').forEach(function(navLink) {
+            navLink.addEventListener('click', function() {
+                document.querySelector('.navlist').classList.remove('show');
+                document.querySelector('.navbar').classList.remove('show');
+            });
+        });
+
+        document.querySelector('.close-menu').addEventListener('click', function() {
+            document.querySelector('.navlist').classList.remove('show');
+            document.querySelector('.navbar').classList.remove('show');
+        });
+
+        document.addEventListener('DOMContentLoaded', function () {
+            var video = document.getElementById('spaceVideo');
+
+            var observer = new IntersectionObserver(function(entries) {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        video.play();
+                    } else {
+                        video.pause();
+                    }
+                });
+            }, {
+                threshold: 0.5
+            });
+
+            observer.observe(document.getElementById('space'));
+        });
