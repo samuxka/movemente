f7707ff381fb243e01f2fd234d6997f1a42d05cb
samuxka <samukactto@gmail.com>
2024-05-27 21:54:56 -0300

    deus me ajude

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,19 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 60)
 
 })
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction(){
+    const backToTopBtn = document.getElementById("backToTopBtn")
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        backToTopBtn.style.display = "block"
+    }else{
+        backToTopBtn.style.display = "none"
+    }
+}
+
+function topFunction(){
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
